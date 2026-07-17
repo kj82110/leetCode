ec4c4dfd6926921e5523541ba11a1717e3d4757b
Kavya Joshi <kj82110@gmail.com>
2026-07-17 15:46:57 +0530

    Time: 16 ms (80.55%) | Memory: 81.5 MB (62.00%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        heights.push_back(0);
         stack<int> st;
-        int ans = 0, n = heights.size();
+        int ans = 0;
 
-        for (int i = 0; i <= n; i++) {
-            while (!st.empty() && (i == n || heights[st.top()] > heights[i])) {
+        for (int i = 0; i < heights.size(); i++) {
+            while (!st.empty() && heights[st.top()] > heights[i]) {
                 int h = heights[st.top()];
                 st.pop();
+
                 int w = st.empty() ? i : i - st.top() - 1;
                 ans = max(ans, h * w);
             }
