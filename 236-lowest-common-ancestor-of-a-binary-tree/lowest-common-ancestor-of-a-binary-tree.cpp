f38e5ffb4b5c2bcc4c60f9f0685e007c379e2447
Kavya Joshi <kj82110@gmail.com>
2026-07-20 16:34:06 +0530

    Time: 80 ms (5.29%) | Memory: 44.1 MB (10.83%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,45 +1,17 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
+    inline TreeNode* dfs(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q)
+            return root;
+
+        TreeNode* l = dfs(root->left, p, q);
+        TreeNode* r = dfs(root->right, p, q);
+
+        if (l && r) return root;
+        return l ? l : r;
+    }
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        unordered_map<TreeNode*, TreeNode*> parent;
-        stack<TreeNode*> st;
-
-        parent[root] = nullptr;
-        st.push(root);
-
-        while (!parent.count(p) || !parent.count(q)) {
-            TreeNode* node = st.top();
-            st.pop();
-
-            if (node->left) {
-                parent[node->left] = node;
-                st.push(node->left);
-            }
-            if (node->right) {
-                parent[node->right] = node;
-                st.push(node->right);
-            }
-        }
-
-        unordered_set<TreeNode*> ancestors;
-
-        while (p) {
-            ancestors.insert(p);
-            p = parent[p];
-        }
-
-        while (!ancestors.count(q))
-            q = parent[q];
-
-        return q;
+        return dfs(root, p, q);
     }
 };
