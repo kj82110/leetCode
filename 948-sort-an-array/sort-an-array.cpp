a5d1154fcacaac9b152d7d5002da0b1b6e5d93e4
Kavya Joshi <kj82110@gmail.com>
2026-07-27 14:51:15 +0530

    Time: 17 ms (91.62%) | Memory: 71.4 MB (69.88%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,41 +1,49 @@
+const int LEN = 5e5 + 5;
+const int BUCK_SIZE = (1 << 16);
+const int REM = (1 << 16) - 1;
+
+typedef unsigned int ui;
+
+int N;
+int psa[BUCK_SIZE];
+ui arr[LEN];
+
+inline void sort(ui *a) {
+    for(int i = 0; i < N; ++i)
+        a[i] ^= (1 << 31);
+
+    memset(psa, 0, sizeof(psa));
+
+    for(int i = 0; i < N; ++i)
+        psa[a[i] & REM]++;
+
+    for(int i = 1; i < BUCK_SIZE; ++i)
+        psa[i] += psa[i - 1];
+
+    for(int i = N-1; i >= 0; --i)
+        arr[--psa[a[i] & REM]] = a[i];
+    
+    memset(psa, 0, sizeof(psa));
+
+    for(int i = 0; i < N; ++i)
+        psa[(arr[i] >> 16) & REM]++;
+
+    for(int i = 1; i < BUCK_SIZE; ++i)
+        psa[i] += psa[i - 1];
+
+    for(int i = N-1; i >= 0; --i)
+        a[--psa[(arr[i] >> 16) & REM]] = arr[i];
+    
+    for (int i = 0; i < N; i ++) 
+        a[i] ^= (1 << 31);
+    
+}
+
 class Solution {
 public:
-    void merge(vector<int>& nums, int left, int mid, int right) {
-        vector<int> temp;
-        int i = left;
-        int j = mid + 1;
-
-        while (i <= mid && j <= right) {
-            if (nums[i] <= nums[j])
-                temp.push_back(nums[i++]);
-            else
-                temp.push_back(nums[j++]);
-        }
-
-        while (i <= mid)
-            temp.push_back(nums[i++]);
-
-        while (j <= right)
-            temp.push_back(nums[j++]);
-
-        for (int k = 0; k < temp.size(); k++)
-            nums[left + k] = temp[k];
-    }
-
-    void mergeSort(vector<int>& nums, int left, int right) {
-        if (left >= right)
-            return;
-
-        int mid = left + (right - left) / 2;
-
-        mergeSort(nums, left, mid);
-        mergeSort(nums, mid + 1, right);
-
-        merge(nums, left, mid, right);
-    }
-
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size() - 1);
+        N = nums.size();
+        sort((ui*) &(*nums.begin()));
         return nums;
     }
 };
